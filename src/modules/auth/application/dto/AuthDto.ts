30bbd89aff45a6961491673c1be53da48a0dca04
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-25 21:07:28 -0300

    dtos de salida en application

--- src/modules/auth/application/dto/AuthDto.ts
+++ src/modules/auth/application/dto/AuthDto.ts
@@ -22,3 +22,33 @@ export interface ChangePasswordDto {
   currentPassword: string;
   newPassword: string;
 }
+
+//Dtos de salida
+
+export interface UserDto {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  isActive: boolean;
+  profilePicture?: string;
+  role: RoleDto;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface RoleDto {
+  id: string;
+  name: string;
+  description?: string;
+}
+
+export interface LoginResponseDto {
+  token: string;
+  refreshToken: string;
+  user: UserDto;
+}
+
+export interface RefreshTokenDto {
+  refreshToken: string;
+}
